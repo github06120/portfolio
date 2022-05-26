2becb7468ea178003b33211d2be269dbbec15e48
unknown <core.maildesu@yahoo.co.jp>
2022-05-26 12:51:02 +0900

    やあ

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,14 +7,15 @@ import work2 from './img/work2.png';
 import work3 from './img/work3.png';
 import work4 from './img/work4.png';
 import work5 from './img/work5.png';
-import profileImg from './img/aboutTitle.png'
+import work6 from './img/work6.png';
+import profileImg from './img/aboutTitle.png';
 
 class Home extends React.Component {
   render() {
     return (
       <div class="home">
 
-        <h1 className="title">Profile</h1>
+        <div className="title">Profile</div>
 
         <div className="profile">
           <img src={profileImg} className="img"></img>
@@ -22,19 +23,21 @@ class Home extends React.Component {
                              中京大学工学部　情報工学科　４年　森山　廣大です。<br></br>
                              <br></br>
                              Unityを使ってゲーム制作しています。<br></br>
-                             好きなものは、スノーボード・サッカー・ゲームです。<br></br>
+                             好きなものは、プログラミング・3Dアート・スノーボード・サッカー・マジックです。<br></br>
           </div>
         </div>
 
 
-        <h1 className="title">業績</h1>
+        <div className="title">業績</div>
 
         <div className="performance">
-          <div className="p">「ARを用いた体感型学習支援ゲームの開発」　情報処理学会 　第８４回全国大会 　　　　　　　　　　　　２０２２  ０３. ０５</div>
-          <div className="p">「モーション認識を用いた運動型AR学習ゲームの開発」　情報処理学会 　第８４回全国大会 　　　　　　　２０２２. ０３. ０５</div>
+          <a href="https://www.ipsj.or.jp/event/taikai/84/ipsj_web2022/data/pdf/6ZH-07.html" className="a">「ARを用いた体感型学習支援ゲームの開発」　情報処理学会 　第８４回全国大会</a>
+          　　　　　　 　　　２０２２  ０３. ０５
+          <a href="https://www.ipsj.or.jp/event/taikai/84/ipsj_web2022/data/pdf/6ZH-07.html" className="a">「モーション認識を用いた運動型AR学習ゲームの開発」　情報処理学会 　第８４回全国大会 </a>
+          　　　２０２２. ０３. ０５
         </div>
 
-        <h1 className="title">開発ソフトウェア</h1>
+        <div className="title">開発ソフトウェア</div>
 
         <div className="works">
 
@@ -47,20 +50,37 @@ class Home extends React.Component {
           </div>
 
           <div className="work">
-            <div className="work-title">AR空間での英語学習ゲームアプリ<br></br>～Listeningモード～</div>
+          <div className="work-title"><br></br></div>
+          <iframe src="https://www.youtube.com/embed/m70bJhvTf3U"allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className="videohome"></iframe>
+          </div>
+
+          <div className="work">
+            <div className="work-title">AR空間での英語学習ゲームアプリ<br></br>～Listeningモード・Speakingモード～</div>
+            <a className="work-p">Listeningモード</a>
             <img src={work3} className="work3"></img>
             <div className="work-a">個別指導塾のアルバイト中、英語が身についていない中学生が多く<br></br>効率よく楽しく学習できる英語学習ゲームを制作。<br></br>AR空間での運動を取り入れ、学習の効率を向上する工夫を行った。<br></br>Listeningモードでは1分間で読まれる英単語の物体をタッチして<br></br>得点を競うゲームです。<br></br></div>
             <a href="https://youtu.be/rg_QvEbn9NI"><button class="button">Demo動画</button></a>
             <a href="https://drive.google.com/file/d/1BFj4U2ehf9Xvzae860qtZ54z5J9So1q4/view?usp=sharing"><button class="button">フィードバック</button></a>
+            
           </div>
 
           <div className="work">
-            <div className="work-title">AR空間での英語学習ゲームアプリ<br></br>～Speakingモード～</div>
+            <div className="work-title"><br></br><br></br></div>
+            <a className="work-p">Speakingモード</a>
             <img src={work4} className="work4"></img>
             <div className="work-a">左記と同様の制作物のSpeakingモード<br></br>Speakingモードでは1分間でboxから出てくる物体を英語で発声し<br></br>得点を競うゲームです。<br></br></div>
             <a href="https://youtu.be/NGzw6G9kUS4"><button class="button">Demo動画</button></a>
             <a href="https://drive.google.com/file/d/1BFj4U2ehf9Xvzae860qtZ54z5J9So1q4/view?usp=sharing"><button class="button">フィードバック</button></a>
+            <a href=""><button class="button">Git hub リンク</button></a>
           </div>
+          
+          <div className="work">
+          <div className="work-title">スマートフォンのジャイロを使用した男女遊べるTPS (チーム開発)</div>
+            <img src={work6} className="work6"></img>
+            <div className="work-a">お菓子を題材にスマートフォンのジャイロで<br></br>視点移動する男女一緒に遊べるTPSを4人チームで開発。<br></br><br></br>（只今開発途中）</div>
+            
+          </div>
+
 
           <div className="work">
             <div className="work-title">Blenderを用いてオリジナルキャラクター制作</div>
@@ -70,7 +90,6 @@ class Home extends React.Component {
 
         </div>
         
-        <iframe src="https://www.youtube.com/embed/m70bJhvTf3U"allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className="videohome"></iframe>
       </div>
     );
   }
